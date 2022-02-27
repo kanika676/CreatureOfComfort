f9e83a2885eb5b19e37a2b82737b758770daabd7
Kanika <kanika.19jics100@jietjodhpur.ac.in>
2022-02-27 21:53:58 +0530

    update: Translator web app

---
+++ app.js
@@ -0,0 +1,33 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+
+//var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+var serverURL = "https://api.funtranslations.com/translate/shakespeare.json"
+function getTranslationURL(text){
+    return serverURL + "?" + "text=" + text
+}
+
+function errorHandler(error){
+   console.log("error occured", error)
+   alert("Something wrong with server! try again after sometime") 
+} 
+
+function clickHandler()
+    {
+        
+        var inputText = txtInput.value; //taking input.
+        //calling server
+        fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText
+            console.log(json.contents.translated)})
+        .catch(errorHandler)
+    };
+
+
+btnTranslate.addEventListener("click", clickHandler)
+
+
